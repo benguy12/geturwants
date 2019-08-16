84982fce4e6dd49c4674c02b81f32ac738cf464b
Victor Benison <emediongbenison@yahoo.com>
2019-08-16 19:39:15 +0100

    change files

---
+++ details-modal-Foldingmultifunctiontoiletstool.php
@@ -0,0 +1,58 @@
+<div class="modal fade details-1" id="details-1" tableindex="-1"role="dialog" aria-labelledby="details-1" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="modal-title text-center">Folding Multi Function Toilet Stool</h4>
+      </div>
+      <div class="modal-body">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="center-block">
+                <img src="images/toilet-stool-1.jpg" alt="Toilet Stool" class="details img-responsive" />
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <h4>Details</h4>
+              <p>An epic addition to Squat bathroom revolution, the 7-inch foldable squatting stool features an exclusive foldable design for fast and easy storage. Banish bathroom eyesores for good by tucking it behind your toilet or inside a cabinet until nature calls. Minimalist inspired, it's the perfect travel companion for achieving a better go while on the go! Choose the stark white color for a clean, crisp look or tap into the dark side with the midnight black version. The foldable squatting stool provides an ideal blend between high-end design and ultimate portability, all while giving you a healthier, more natural way to go.
+              </p>
+              <hr />
+              <p><strong>Price: &#8358;9,000</strong></p>
+              <p><strong>*Specifications:*</strong></p>
+              <ul>
+                <li>Color: White</li>
+                <li>Size: 40 x 25 x 17.5 cm</li>
+                <li>Material: Polypropylene</li>
+                <li>Package Include: 1 x Folding Multi-Function Toilet Stool</li>
+              </ul>
+              <form action="add_cart.php" method="post">
+                <div class="form-group">
+                  <div class="col-xs-3">
+                    <label for="quantiy" id="quantity-label">Quantity</label>
+                    <input type="text" class="form-control" id="quantity" name="quantity" />
+                  </div><br /><br /><br />
+                  <div class="form-group">
+                    <label for="size">Size:</label>
+                    <select name="size" id="size" class="form-control">
+                      <option value=""></option>
+                      <option value="28">28</option>
+                      <option value="32">32</option>
+                      <option value="36">36</option>
+                    </select>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-default" data-dismiss="modal">Close</button>
+        <button class="btn btn-warning" type="submit"><span class="glyphicon glyphicon-shopping-cart"</span>Add To Cart</button>
+      </div>
+    </div>
+  </div>
+</div>
